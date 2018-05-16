735cc39e3fbad8d910ea86a790a62e5b2c596039
amos wachira <amoswachira50@gmail.com>
2018-05-16 12:33:07 +0300

    initial express server with route files

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/posts/test
+// @desc    test posts route
+// @access   public
+router.get('/test', (req, res) => res.json({
+    msg: 'posts works'
+}));
+
+module.exports = router;
