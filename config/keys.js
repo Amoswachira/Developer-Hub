d44ffcc78a782045c8f9be68174f4f2feb1424a4
amos wachira <amoswachira50@gmail.com>
2018-08-09 16:26:36 +0300

    added profile display on client side

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     // production db
-    // mongoURI: 'mongodb://devflex:devFlex!@ds237120.mlab.com:37120/devflex',
+    mongoURI: 'mongodb://devflex:devFlex!@ds237120.mlab.com:37120/devflex',
 
     // localhost db development
-    mongoURI: 'mongodb://localhost:27017/devflex',
+    // mongoURI: 'mongodb://localhost:27017/devflex',
     secretOrKey: 'justakey'
 };
