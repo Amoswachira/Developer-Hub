4b4ad2e8fa8e40ea59f14587c1e43b53cec29f1d
amos wachira <amoswachira50@gmail.com>
2018-08-09 17:25:31 +0300

    added post feed, comments and fixed deployment issues

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
-module.exports = {
-    // production db
-    mongoURI: 'mongodb://devflex:devFlex!@ds237120.mlab.com:37120/devflex',
 
-    // localhost db development
-    // mongoURI: 'mongodb://localhost:27017/devflex',
-    secretOrKey: 'justakey'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mongoURI: 'mongodb://devhubuser:devhubuser101@ds217092.mlab.com:17092/devhub',
+  secretOrKey: 'secret'
+};
+
+// module.exports = {
+//     // production db
+//     mongoURI: 'mongodb://devhubuser:devhubuser101@ds217092.mlab.com:17092/devhub',
+//     // mongodb://devhubuser:devhubuser101@ds217092.mlab.com:17092/devhub
+
+//     // localhost db development
+//     // mongoURI: 'mongodb://localhost:27017/devhub',
+//     secretOrKey: 'justakey'
+// };
