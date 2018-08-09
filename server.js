d516edb90aed24476bfe10e97ec6edd8e5c7d2a5
amos wachira <amoswachira50@gmail.com>
2018-08-09 18:15:29 +0300

    post deployment fixed issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
@@ -8,47 +8,42 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
-
 const app = express();
 
-// Body parser middlewara
-app.use(bodyparser.urlencoded({
-    extended: false
-}));
-app.use(bodyparser.json());
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// db config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-// connect  to MongoDb
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('mongoDB connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world "));
-
-// passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-// passport config
+// Passport Config
 require('./config/passport')(passport);
 
-// use routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
